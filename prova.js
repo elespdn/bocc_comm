3c2aa6ff713adbc1ea70bd063a28fe59981163f4
elespdn <spadinielena@gmail.com>
2017-02-27 13:51:14 +0100

    first commit

---
+++ prova.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    var t = $('#example').DataTable();
+    var counter = 1;
+ 
+    $('#addRow').on( 'click', function () {
+        t.row.add( [
+            counter +'.1',
+            counter +'.2',
+            counter +'.3',
+            counter +'.4',
+            counter +'.5'
+        ] ).draw( false );
+ 
+        counter++;
+    } );
